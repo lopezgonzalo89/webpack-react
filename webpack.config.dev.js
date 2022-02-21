e1eb6ae82e435b45f4bd1e03f4043a5263abb658
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2022-02-20 23:20:27 -0300

    dev and prod

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
   },
-  mode: "production",
+  mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-    },
   },
   module: {
     rules: [
@@ -47,10 +39,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
     }),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  devServer: {
+    open: true,
+    static: path.join(__dirname, "dist"),
+    compress: true,
   },
 };
