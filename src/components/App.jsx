fff9ab5b715b2b2702a45693dde575420848be83
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2022-02-20 23:23:39 -0300

    updated test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <h1>Hello, React!</h1>
-  )
-}
+    <>
+      <h1>Hello, React!</h1>
+      <h4>Actualizado</h4>
+    </>
+  );
+};
 
-export default App
+export default App;
